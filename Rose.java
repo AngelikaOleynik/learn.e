f409d43fc288b6ae64145b5b290722b0a0d1554e
Angelika <oleinik38@gmail.com>
2020-04-14 12:59:36 +0300

    add "asd" in three

--- Rose.java
+++ Rose.java
@@ -2,6 +2,7 @@ public class Rose {
     public static void main(String[] args) {
         System.out.println("123");
         System.out.println("321");
+        System.out.println("asd");
 
     }
 }
