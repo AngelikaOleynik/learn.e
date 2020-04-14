b4f41e754f8d80231850721458f6b32502b9069a
Angelika <oleinik38@gmail.com>
2020-04-14 12:32:51 +0300

    revert commit

--- Rose.java
+++ Rose.java
@@ -3,6 +3,11 @@ public class Rose {
         System.out.println("123");
         System.out.println("321");
         System.out.println("456");
-
+//<<<<<<< HEAD
+//        System.out.println("456");
+//
+//=======
+//>>>>>>> parent of e23b526... Fast-forwarded three to one
+//    }
     }
 }
